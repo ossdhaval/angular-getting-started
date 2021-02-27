b62e6e9fb3cccdd4c312e1401bf9b6af2573886c
Dhaval Desai <dhavaltdesai@yahoo.com>
2021-02-27 17:40:16 +0530

    add stars for rating using nested component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list-component';
+import { StarComponent} from './shared/star.component'
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule,
